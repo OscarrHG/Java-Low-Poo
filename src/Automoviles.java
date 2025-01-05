7e4151b12b86613d8d1732262d3a99daa0332b89
OscarrHG <hgoscar3@gmail.com>
2025-01-04 23:37:42 -0500

    Subida inicial del proyecto POO

---
+++ src/Automoviles.java
@@ -0,0 +1,12 @@
+public class Automoviles {
+    String marca = "Honda";
+    String modelo = "CRV";
+    int año = 2025;
+    int cant_ruedas = 4;
+    boolean camara = true;
+
+
+    public static void main(String[] args) {
+
+    }
+}
